edf75275ccbcb406790c0d071e48b9b4301d18f4
Ovimonno Chy Roni <oceanc9361@gmail.com>
2022-09-06 23:04:34 +0600

    ValoBasa

---
+++ javascript.js
@@ -0,0 +1,34 @@
+function search_Data(){
+    const searchResult = document.getElementById("resultSearch").value;
+
+    fetch (`https://api.lyrics.ovh/suggest/${searchResult}`)
+        .then(res =>res.json())
+        .then(data => findData(data))
+        alert("Data Not Found")
+        
+}
+function findData(song){
+    // console.log(song.data)
+    const AllData = song.data;
+    // console.log(AllData)
+    for (let i = 0; i < AllData.length; i++) {
+        const AllDatabase = AllData[i];
+        // console.log(AllDatabase.title);
+        const getData = document.getElementById("getSong");
+        const setData = document.createElement('div');
+        setData.className = "single-result row align-items-center my-3 p-3";
+        setData.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${AllDatabase.title}</h3>
+            <p class="author lead">Album by <span>${AllDatabase.artist.name}</span></p>
+            <audio controls>
+            <source src="${AllDatabase.preview}" type ="audio/mpeg">
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success">Get Lyrics</button>
+        </div>
+        `
+        getData.appendChild(setData);
+    }
+}
